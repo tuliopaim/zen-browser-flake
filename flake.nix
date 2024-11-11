7a6f5bb2c210c1b8666c18f493a6e6201ef7bcd4
tuliopaim <tutpaim@gmail.com>
2024-11-11 08:44:03 -0300

    .19

--- flake.nix
+++ flake.nix
@@ -8,11 +8,11 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.1-a.18";
+      version = "1.0.1-a.19";
       downloadUrl = {
         "specific" = {
           url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-          sha256 = "sha256:1n6z8wx8w7dhir8vaqbc19ibhsc6g6rma4kvx2nw04l8b4hxg9bg";
+          sha256 = "sha256:1g7nq1yfaya97m43vnkjj1nd9g570viy8hj45c523hcyr1z92rjq";
         };
       };
 
